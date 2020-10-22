ee2a4717898e7f39b247e8160e0efb0d76b6c59c
Brijesh <brijeshpandey18@gmail.com>
2020-10-22 16:31:39 +0530

    strangeString using dequeu

---
+++ src/Queues/strangeString.java
@@ -0,0 +1,44 @@
+package Queues;
+import java.util.*;
+import java.io.*;
+/*
+The question is super small and super simple.
+You are given an integer n. Initially you have an empty string. You need to construct the string of length n with the following rules:
+1. Insert the first character in the beginning of string, the second in the end, the third in the beginning, fourth in the end, and so on.
+2. The first character should be 'a', followed by 'b', 'c', and so on. 'z' will be followed by 'a'.
+Input
+The first and the only line of input contains a single number n.
+
+Constraints
+1 <= n <= 500000
+Output
+Output the generated string.
+Example
+Sample Input
+4
+
+Sample Output
+cabd
+
+Sample Input
+30
+
+Sample Output
+caywusqomkigecabdfhjlnprtvxzbd
+
+Explanation
+In the first case the string transforms as follows: "" -> "a" -> "ab" -> "cab" -> "cabd"
+ */
+
+public class strangeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        char ch = 'a';
+        while (ch <= length){
+            System.out.println(ch);
+            ch++;
+        }
+
+    }
+}
